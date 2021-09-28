cdd64733c1951132435d1705f9551813fb266fc2
Desginer123 <hotsru12@gmail.com>
2021-09-28 12:16:43 +0800

    second commit

--- parser.py
+++ parser.py
@@ -14,20 +14,20 @@ with open(r"C:\Users\user\Desktop\pyparser\list.txt", 'a', encoding='utf-8') as 
         items = soup.find_all('div', class_='recipe_l')
         for n, i in enumerate(items, start =1):
             itemName = i.find('div', class_='title').text.strip()
-            itemPrice = i.find('div', class_='ingr_str').text.strip()
+            itemIngredients = i.find('div', class_='ingr_str').text.strip()
             photo = i.find(class_ = "foto")
             image = photo.find("img")
-            if itemPrice is None:
+            if itemIngredients is None:
                break
             else:
                 if image is None:
-                    file.write(str(n) + ": " + itemName + '\n' + 'Ингридиенты:' + itemPrice + '\n' + 'Имя картинки:' +  itemName + '\n')
+                    file.write(str(n) + ": " + itemName + '\n' + 'Ингридиенты:' + itemIngredients + '\n' + 'Имя картинки:' +  itemName + '\n')
                     print("Скачиваю {}".format(itemName))
                 else:
                     source=image.get("src")
                     relurl = 'https:' + source
                     img_name = str(source.split('/')[-1])
-                    file.write(str(n) + ": " + itemName + '\n' + 'Ингридиенты:' + itemPrice + '\n' + 'Имя картинки:' +  itemName + '\n')
+                    file.write(str(n) + ": " + itemName + '\n' + 'Ингридиенты:' + itemIngredients + '\n' + 'Имя картинки:' +  itemName + '\n')
                     print("Скачиваю {}".format(itemName))
                     formatName = itemName.replace(" ", "_")
                     readyName = formatName.replace('"', '_')
